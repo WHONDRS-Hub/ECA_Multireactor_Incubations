bb76ab49966caf855e5c9c768f9144227abd622d
mmlaan <maggi.laan@gmail.com>
2024-03-05 16:00:23 -0800

    Most recent data files

--- ECA_wrangled_data.R
+++ ECA_wrangled_data.R
@@ -21,121 +21,56 @@ pnnl.user = 'laan208'
 
 # choose file dates to read in 
 
-effect.date = '2023-11-08'
-respiration.date = '2023-11-08'
-removed.respiration.date = '2023-11-13'
-mg.kg.respiration.date = '2023-12-05'
-grav.date = '2023-12-01'
+respiration.date = '2024-03-05'
+respiration.summary = '2024-03-05'
+grav.date = '2024-01-29'
+grav.summary = '01-29-2024'
+fe.date = '03-04-2024'
+fe.summary = '03-05-2024'
+atp.date = '01-26-2024'
+atp.summary = '03-05-2024'
 
 #Read in all data
 setwd(paste0("C:/Users/",pnnl.user,"/PNNL/Core Richland and Sequim Lab-Field Team - Documents/Data Generation and Files/ECA/"))
 
-#effect size - change date to most recent
-effect_size <- read_csv(paste0("Optode multi reactor/Optode_multi_reactor_incubation/rates/ReadyForBoye/ECA_Effect_Size_ReadyForBoye_",effect.date,".csv"))
+#All Respiration Rates
+all_respiration <- read.csv(paste0("INC/03_ProcessedData/ECA_Sediment_Incubations_Respiration_Rates_ReadyForBoye_",respiration.date,".csv"))
 
-
-#Respiration rates with removals from dist matrix to calculate effect size 
-
-#respiration <- read.csv(paste0("C:/Users/laan208/PNNL/Core Richland and Sequim Lab-Field Team - Documents/Data Generation and Files/ECA/Optode multi reactor/Optode_multi_reactor_incubation/rates/ReadyForBoye/ECA_Sediment_Incubations_Removed_Respiration_Rates_",removed.respiration.date,".csv"))
-
-#all_respiration <- read.csv(paste0("C:/Users/laan208/PNNL/Core Richland and Sequim Lab-Field Team - Documents/Data Generation and Files/ECA/Optode multi reactor/Optode_multi_reactor_incubation/rates/ReadyForBoye/ECA_Sediment_Incubations_Respiration_Rates_ReadyForBoye_",respiration.date,".csv"))
-
-all_respiration <- read.csv(paste0("C:/Users/laan208/PNNL/Core Richland and Sequim Lab-Field Team - Documents/Data Generation and Files/ECA/Optode multi reactor/Optode_multi_reactor_incubation/rates/ReadyForBoye/ECA_Sediment_Incubations_mg_kg_rates_laan208_on_",mg.kg.respiration.date,".csv"))
-
-all_respiration <- all_respiration %>% 
-  dplyr::select(c(Sample_Name, Respiration_Rate_mg_DO_per_L_per_H, Respiration_Rate_mg_DO_per_kg_per_H, mass_water))
+mean_respiration <- read.csv(paste0("INC/03_ProcessedData/ECA_Sediment_Incubations_Respiration_Rates_Summary_ReadyForBoye_",respiration.summary,".csv"))
 
 #ECA Iron
-iron <- read_csv(paste0("Fe/03_ProcessedData/EC_SFE_ReadyForBoye_12-05-2023.csv"))
+all_iron <- read_csv(paste0("Fe/03_ProcessedData/EC_ReadyForBoye_",fe.date,".csv"))
 
-iron <- iron %>% 
-  dplyr::select(c(Sample_Name, Fe_mg_per_L, Fe_mg_per_kg)) %>% 
-separate(Sample_Name, into = c("EC", "Site", "INC"), sep = "_", remove = FALSE)
-
-
-iron_samples <- iron %>% 
-  separate(INC, c("Replicate", "Analytical"), sep = -1) %>% 
-  filter(!grepl("LOD", Fe_mg_per_L)) %>% 
-  filter(Fe_mg_per_L >= 0) %>% 
-  group_by(Site, Replicate) %>% 
-  mutate(Fe_mg_per_L = mean(as.numeric(Fe_mg_per_L))) %>% 
-  mutate(Fe_mg_per_kg = mean(as.numeric(Fe_mg_per_kg))) %>% 
-  unite(Sample_Name, c(EC:Replicate), sep = "_") %>% 
-  distinct(Sample_Name, .keep_all = TRUE) %>% 
-  dplyr::select(-c(Analytical)) %>% 
-  mutate(Sample_Name = str_replace(Sample_Name, "SFE", "INC"))
-  
+mean_iron <- read_csv(paste0("Fe/03_ProcessedData/EC_SFE_Summary_ReadyForBoye_",fe.summary,".csv"))
 
 #ICON Grain Size
-grain <- read.csv("C:/Github/ECA_Multireactor_Incubations/Data/v2_CM_SSS_Sediment_Grain_Size.csv", skip = 2, header = TRUE)
+grain <- read.csv("C:/Github/ECA_Multireactor_Incubations/Data/v3_CM_SSS_Sediment_Grain_Size.csv", skip = 2, header = TRUE)
 
-grain <- grain %>% 
+grain_all <- grain %>% 
   filter(!row_number() %in% c(1:11)) %>% 
-  dplyr::select(-c(Field_Name, Material)) 
-
-grain_new <- read.csv(paste0("C:/Users/",pnnl.user,"/PNNL/Core Richland and Sequim Lab-Field Team - Documents/Data Generation and Files/ICON_ModEx_SSS/09_Grain_Size/03_ProcessedData/20230721_Grain_Size_SBR_RC4_CM_R21/20230721_Data_Processed_Grain_Size_SBR_RC4_CM_R21.csv"))
-
-grain <- grain %>% 
-  mutate(Sample_ID = Sample_Name) %>% 
-  dplyr::select(-c(Sample_Name))
-
-grain_all <- rbind(grain, grain_new)
-
-grain_all$Sample_ID <- str_replace(grain_all$Sample_ID, "CM", "EC") 
-
-grain_all <- grain_all %>% 
-  filter(!grepl("SSS", Sample_ID)) %>% 
-  separate(Sample_ID, into = c("EC", "Kit", "GRN"), sep = "_") %>% 
-  unite("Sample_ID", EC:Kit, sep = "_") %>% 
-  dplyr::select(-c(GRN)) %>% 
-  filter(!grepl("NA", Sample_ID)) %>% 
+  dplyr::select(-c(Field_Name, Material)) %>% 
+  filter(!grepl("SSS", Sample_Name)) %>% 
+  filter(Sample_Name != "") %>% 
+  mutate(Sample_Name = str_replace(Sample_Name, "CM", "EC")) %>% 
+  mutate(Sample_Name = str_remove(Sample_Name, "_GRN")) %>% 
   mutate_at(c("Percent_Fine_Sand", "Percent_Med_Sand", "Percent_Coarse_Sand", "Percent_Tot_Sand", "Percent_Silt", "Percent_Clay"), as.numeric)
 
+ssa <- read.csv("C:/GitHub/ECA_Multireactor_Incubations/Data/CM_SSS_Sediment_Specific_Surface_Area.csv", skip = 2, header = TRUE)
 
-ssa <- read_csv(paste0("C:/GitHub/ECA_Multireactor_Incubations/Data/eca_ssa_predatapackage.csv"))
+ssa_clean = ssa %>% 
+  filter(!row_number() %in% c(1:11)) %>% 
+  dplyr::select(-c(Field_Name, Material, IGSN)) %>% 
+  filter(!grepl("SSS", Sample_Name)) %>% 
+  filter(Sample_Name != "") %>% 
+  mutate(Sample_Name = str_replace(Sample_Name, "CM", "EC")) %>% 
+  separate(Sample_Name, c("Sample_Name", "Rep"), sep = -6)
 
-ssa <- ssa %>% 
-  separate(Parent_ID, c("EC", "Site"), sep = "_")
-
-for (i in 1:nrow(ssa)){
-  
-  if (str_count(ssa$Site[i], "[0-9]") <= 2){
-    
-    ssa$Site[i] = paste0("0", ssa$Site[i])
-    
-  }
-  
-  else {
-    
-    ssa$Site[i] = ssa$Site[i]
-  }
-  
-}
-
-ssa <- ssa %>% 
-  unite(Sample_ID, c("EC", "Site"), sep = "_")
-
-mean_ssa <- ssa %>% 
-  group_by(Sample_ID) %>% 
-  mutate(average_ssa = mean(ssa_m2_g)) %>% 
-  distinct(Sample_ID, .keep_all = TRUE) %>% 
-  dplyr::select(Sample_ID, average_ssa)
-
-
-
-#All incubation pH, SpC, temp
-chemistry <- read_csv("INC/03_ProcessedData/SpC_pH_Temp.csv")
-
-map_corr = chemistry %>% 
-  dplyr::select(c(Sample_Name, SpC, Temp, pH)) %>% 
-  filter(!grepl("EV", Sample_Name))
-
-chem_all = map_corr %>% 
-  separate(Sample_Name, c("ECA"
-                          , "kit", "Analysis"), sep = "_", remove = FALSE) %>% 
-  mutate(Treat = case_when(grepl("W",Analysis)~"Wet",
-                           grepl("D", Analysis) ~"Dry"))
-
+mean_ssa <- ssa_clean %>% 
+  filter(Specific_Surface_Area_m2_per_g != -9999) %>% 
+  filter(!grepl("Negative", Specific_Surface_Area_m2_per_g)) %>%
+  mutate(Specific_Surface_Area_m2_per_g = as.numeric(Specific_Surface_Area_m2_per_g)) %>%
+  group_by(Sample_Name) %>%
+  summarise(mean_ssa = mean(Specific_Surface_Area_m2_per_g, na.rm = TRUE))
 
 #Gravimetric Moisture
 
