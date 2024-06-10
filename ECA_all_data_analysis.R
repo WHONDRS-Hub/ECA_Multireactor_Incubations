4d2bbba187eebe551706fdfce48534fc3779f975
mmlaan <maggi.laan@gmail.com>
2024-06-10 15:51:25 -0700

    Update code to combine figures

--- ECA_all_data_analysis.R
+++ ECA_all_data_analysis.R
@@ -316,7 +316,7 @@ corrplot(cube_effect_samples_corr,type = "upper", method = "number", tl.col = "b
 # 
  dev.off()
 
-## Scatter Plots ####
+## Scatter Plots combined with PCA ####
 
 ##Fe vs. Effect ##
 
@@ -371,7 +371,7 @@ atp_cube = ggplot(cube_effect_data_corr, aes(x = Cube_ATP_pmol_g_Diff, y = Cube_
 
 ##Fine Sand vs. Effect ##
 
-#png(file = paste0("C:/Github/ECA_Multireactor_Incubations/Physical_Manuscript_Figures/", as.character(Sys.Date()),"_Cube_Median_Effect_vs_ATP_Scatter.png"), width = 6, height = 6, units = "in", res = 300)
+png(file = paste0("C:/Github/ECA_Multireactor_Incubations/Physical_Manuscript_Figures/", as.character(Sys.Date()),"_Cube_Median_Effect_vs_Fine_Sand_Scatter.png"), width = 6, height = 6, units = "in", res = 300)
 
 fs_cube = ggplot(cube_effect_data_corr, aes(x = Cube_Fine_Sand, y = Cube_Effect_Size)) +
   geom_point() +
@@ -383,6 +383,10 @@ fs_cube = ggplot(cube_effect_data_corr, aes(x = Cube_Fine_Sand, y = Cube_Effect_
   xlab("Cubed Root Fine Sand (%)") +
   ylab("Cubed Root Effect Size (mg/kg) (Wet - Dry)")+ 
   theme(text = element_text(size = 12)) 
+
+fs_cube
+
+dev.off()
 
 # silt_cube = ggplot(cube_effect_data_corr, aes(x = Cube_Silt, y = Cube_Effect_Size)) +
 #   geom_point() +
@@ -455,11 +459,36 @@ grav_cube = ggplot(cube_effect_data_corr, aes(x = Cube_Dry_LostGravMoi, y = Cube
   ylab("Cubed Root Effect Size (mg/kg) (Wet - Dry)")+ 
   theme(text = element_text(size = 12)) 
 
+grav_cube
+
 dev.off()
 
 
 
-## Effect Size PCA ####
+## Effect Size PCA combined figure ####
+
+fine_sand_image = image_read("C:/GitHub/ECA_Multireactor_Incubations/Physical_Manuscript_Figures/2024-06-10_Cube_Median_Effect_vs_Fine_Sand_Scatter.png")
+
+fine_sand_label_image = image_annotate(fine_sand_image, "B", size = 65, location = "+30+20", color = "black")
+
+fine_sand_scale_image = image_scale(fine_sand_label_image, "65%")
+
+grav_moi_image = image_read("C:/GitHub/ECA_Multireactor_Incubations/Physical_Manuscript_Figures/2024-06-10_Cube_Median_Effect_vs_Fin_Grav_Moi_Scatter.png")
+
+grav_moi_label_image = image_annotate(grav_moi_image, "C", size = 65, location = "+30+20", color = "black")
+
+grav_moi_scale_image = image_scale(grav_moi_label_image, "65%")
+
+scatter_image = image_append(c(fine_sand_scale_image, grav_moi_scale_image), stack = TRUE)
+
+pca_image = image_read("C:/GitHub/ECA_Multireactor_Incubations/Physical_Manuscript_Figures/2024-06-10_Cube_Median_Effect_PCA.png")
+
+pca_label_image = image_annotate(pca_image, "A", size = 65, location = "+30+20", color = "black")
+
+pca_combine_image = image_append(c(pca_label_image, scatter_image))
+
+
+image_write(pca_combine_image, path = "C:/Github/ECA_Multireactor_Incubations/Physical_Manuscript_Figures/2024-06-10_Combined_PCA.png")
 
 effect_data_clean = effect_data %>% 
   select(-c(Rep, diff_median_ATP_nanomol_per_L, diff_median_Respiration_Rate_mg_DO_per_L_per_H, diff_median_Fe_mg_per_L)) %>% 
@@ -482,89 +511,6 @@ effect_data_clean = effect_data %>%
   rename(SSA = median_mean_ssa) %>% 
   relocate(Effect_Size, .before = SpC_Diff) %>% 
   filter(Fe_mg_kg_Diff > -1)
-
-effect_pca <- prcomp(effect_data_clean, scale = TRUE,
-                 center = TRUE, retx = T)
-
-# Summary
-summary(effect_pca)
-
-# See the principal components
-dim(effect_pca$x)
-effect_pca$x
-
-## PCA into LASSO ####
-#Put prop. of variation in DF for LASSO
-
-pca_lasso_data = effect_data_clean %>% 
-  select(-c(Effect_Size))
-
-effect_pca_lasso <- prcomp(pca_lasso_data, scale = TRUE,
-                     center = TRUE, retx = T)
-
-summary(effect_pca_lasso)
-
-pca_scores = as.data.frame(effect_pca_lasso$x)
-
-correlations = cor(pca_lasso_data, pca_scores)
-print(correlations)
-
-pca_scores_scale = as.data.frame(scale(pca_scores))
-
-#mean(pca_scores$PC1)
-#sd(pca_scores$PC1)
-
-
-
-## Set response variable
-resp_pca_lasso <- scale(effect_data_clean$Effect_Size)
-
-## Set predictor variables
-pred_pca_lasso <- data.matrix(pca_scores_scale[, c("PC1",  "PC2",  "PC3", "PC4", "PC5", "PC6", "PC7",  "PC8", "PC9", "PC10", "PC11", "PC12")])
-
-
-
-lasso = cv.glmnet(pred_pca_lasso, resp_pca_lasso, alpha = 1)
-
-best_lambda <- lasso$lambda.min
-best_lambda
-
-plot(lasso)
-
-best_lasso_model <- glmnet(pred_pca_lasso, resp_pca_lasso, alpha = 1, lambda = best_lambda)
-
-coef(best_lasso_model)
-
-resp_predict <- predict(best_lasso_model, s = best_lambda, newx = pred_pca_lasso)
-
-sst <- sum((resp_pca_lasso - mean(resp_pca_lasso))^2)
-sse <- sum((resp_predict - resp_pca_lasso)^2)
-
-rsq = 1 - sse/sst
-
-rsq
-
-
-####
-
-limits = c(-1450,
-           1450)
-
-ind <- get_pca_ind(effect_pca)
-ind
-
-png(file = paste0("C:/Github/ECA_Multireactor_Incubations/Physical_Manuscript_Figures/", as.character(Sys.Date()),"_Median_Effect_PCA.png"), width = 8, height = 8, units = "in", res = 300)
-
-pca = fviz_pca_biplot(effect_pca, col.var = "black",geom = "point"
-)+
-  geom_point(aes(color = effect_data_clean$Effect_Size), size = 3.5)+
-  scale_color_gradient2(limits = limits, low = "firebrick2", mid = "goldenrod2",
-                        high = "dodgerblue2", midpoint = (max(limits)+min(limits))/2) +
-  labs(color = paste0("Wet - Dry Rate"))
-
-pca
-
-dev.off()
 
 ## PCA with Scatter Plots ####
 
@@ -611,118 +557,3 @@ combine_pca
 
 dev.off()
 
-## Cube PCA for LASSO####
-
-# Fe outlier not in analysis
-# Effect Size not in PCA
-
-cube_effect_data_clean = cube_effect_data_corr %>% 
-  #relocate(cube_Effect_Size, .before = cube_SpC_Diff) %>% 
-  filter(Cube_Fe_mg_kg_Diff > -1)
-
-#limits = c(0, 1450)
-#ggplot(effect_data_clean, aes(x = FinGravMoi_Diff, y = Fine_Sand, color = Effect_Size)) +
-#  geom_point() +
-#  scale_color_gradient2(limits = limits, low = "firebrick2", mid = "goldenrod2",
- #                       high = "dodgerblue2", midpoint = (max(limits)+min(limits))/2)
-
-##Run PCA without effect size
-cube_data_pca = cube_effect_data_clean %>% 
-  select(-c(Cube_Effect_Size))
-
-cube_effect_pca <- prcomp(cube_data_pca, scale = TRUE,
-                     center = TRUE, retx = T)
-
-# Summary
-summary(cube_effect_pca)
-
-#cumulative variation
-
-cumulative_var = cumsum(cube_effect_pca$sdev^2)/sum(cube_effect_pca$sdev^2)
-
-pca_plot = data.frame(PC = 1:length(cube_effect_pca$sdev), Cumulative_Variance = cumulative_var)
-
-ggplot(pca_plot, aes(x = PC, y = Cumulative_Variance)) +
-  geom_col()
-
-
-# Look at PCA loadings
-
-loadings = cube_effect_pca$rotation
-
-print(loadings)
-
-loadings_df <- as.data.frame(loadings) %>% 
-  select(c(PC1, PC2, PC3, PC4, PC5)) 
-
-# Plotting the heatmap
-
-row_names <- rownames(loadings_df)
-loadings_df$Variable <- row_names
-
-# Melt the dataframe for plotting
-loadings_melted <- reshape2::melt(loadings_df, id.vars = "Variable")
-
-# Plotting the heatmap
-ggplot(loadings_melted, aes(x = variable, y = Variable, fill = value)) +
-  geom_tile() +
-  scale_fill_gradient2(low = "blue", mid = "white", high = "red", midpoint = 0) +
-  labs(x = "Principal Component", y = "Variable", fill = "Loadings") +
-  ggtitle("PCA Loadings Heatmap")+
-  geom_text(aes(label = round(value, 2)), color = "black", size = 3)
-
-
-
-
-set.seed(42)
-## Set response variable
-yvar <- data.matrix(scale(cube_effect_data_clean$Cube_Effect_Size, center = TRUE, scale = TRUE))
-#yvar <- data.matrix(cube_effect_data_clean$Cube_Effect_Size)
-mean(yvar)
-sd(yvar)
-
-## Set predictor variables
-pca_scores = as.data.frame(scale(cube_effect_pca$x, center = TRUE, scale = TRUE))
-#pca_scores = as.data.frame(cube_effect_pca$x)
-mean(pca_scores$PC1)
-sd(pca_scores$PC1)
-
-xvars <- data.matrix(pca_scores[, c("PC1",  "PC2", "PC3", "PC4", "PC5")])
-
-lasso = cv.glmnet(xvars, yvar, alpha = 1, 
-                  standardize = FALSE, standardize.response = FALSE, intercept = FALSE, nfolds = 5)
-
-best_lambda <- lasso$lambda.min
-best_lambda
-
-plot(lasso)
-
-best_lasso_model <- glmnet(xvars, yvar, alpha = 1, lambda = best_lambda, standardize = FALSE, standardize.response = FALSE, intercept = FALSE)
-
-coef(best_lasso_model)
-
-yvar_predict <- predict(best_lasso_model, s = best_lambda, newx = xvars)
-
-sst <- sum((yvar - mean(yvar))^2)
-sse <- sum((yvar_predict - yvar)^2)
-
-rsq = 1 - sse/sst
-
-rsq
-
-
-cube_limits = c(-12,
-           12)
-
-png(file = paste0("C:/Github/ECA_Multireactor_Incubations/Physical_Manuscript_Figures/", as.character(Sys.Date()),"_Cube_Median_Effect_PCA.png"), width = 8, height = 8, units = "in", res = 300)
-
-cube_pca = fviz_pca_biplot(cube_effect_pca, col.var = "black",geom = "point"
-)+
-  geom_point(aes(color = cube_effect_data_clean$Cube_Effect_Size), size = 3.5)+
-  scale_color_gradient2(limits = cube_limits, low = "firebrick2", mid = "goldenrod2",
-                        high = "dodgerblue2", midpoint = (max(cube_limits)+min(cube_limits))/2) +
-  labs(color = paste0("Cubed Root Effect Size\n(Wet - Dry Rate)"))
-
-cube_pca
-
-dev.off()
