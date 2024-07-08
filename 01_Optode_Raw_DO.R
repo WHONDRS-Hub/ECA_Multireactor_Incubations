0f2d7513917370e93e6a2b60ed3746ef69578f98
mmlaan <maggi.laan@gmail.com>
2024-07-08 15:10:40 -0700

    Updated 01_Optode_Raw_DO.R to resolve informal code review edits
    Closes issue #2

--- 01_Optode_Raw_DO.R
+++ 01_Optode_Raw_DO.R
@@ -5,7 +5,7 @@ library(tidyverse); library(readxl)
 ##### Load data ######
 rm(list=ls());graphics.off()
 
-# Set working directory to data file
+# Set paths to data files and outputs
 
 ## INPUTS ####
 pnnl.user = 'laan208'
@@ -44,13 +44,14 @@ import_data = function(map.path){
   
   filePaths <- filePaths[grepl("results", filePaths)]
   
-  # Remove ECA samples incubated in Jars
+  # Remove ECA samples incubated in Jars. These were done at the beginning of the ECA 2022 experiment, before moving to 50 mL tubes because samples were not drying out sufficiently 
   filePaths <- filePaths[!grepl("EC_01|EC_02|EC_03|EC_04_08|EC_06_07|EC_10_15", filePaths)]
   
     # dat <- 
   do.call(rbind, lapply(filePaths, function(map.path){
     # then add a new column `source` to denote the file name
-    df <- read.csv(map.path, skip = 4)
+    df <- read.csv(map.path, skip = 4, stringsAsFactors =  F)
+
     df[["source_file"]] <- rep(map.path, nrow(df)) # add a column for the source file
     
     df %>%
@@ -130,7 +131,7 @@ cleaned_data <- bind %>%
 
 
 # Pull in .txt files with picture times and combine
-import_data = function(input.path){ 
+import_txt = function(input.path){ 
   
   filePaths <- list.files(path = input.path, recursive = T, pattern = "\\.txt$", full.names = TRUE)
   
@@ -146,37 +147,50 @@ import_data = function(input.path){
     do.call(rbind,mapping)
 }
 
-all.txt = import_data(input.path)  
+all.txt = import_txt(input.path)  
 
 img_all <- all.txt %>% 
   mutate(`filePaths[i]` = str_remove_all(`filePaths[i]`, paste0(input.path, "/"))) %>% 
   separate(col = `filePaths[i]`, into = c("source_file", "photo"), sep = "/")
 
-img_time <- img_all[!grepl("Custom|type|.tif|----", img_all$TIFF.image.set.saved.with.Look.RGB.v0.1),]
+# pull out times and dates of images
+img_time <- img_all[!grepl("Custom|type|.tif|----", img_all$TIFF.image.set.saved.with.Look.RGB.v0.1),] 
 
+# rename date/time column as Time
 img_time <- rename(img_time, Time = TIFF.image.set.saved.with.Look.RGB.v0.1)
 
+# remove AM/PM in Time
 img_time$Time <- gsub('[AMP]','', img_time$Time)
 
+# make time as.POSIXct format
 img_time$Time <- as.POSIXct(img_time$Time, format = "%m/%d/%Y %H:%M:%S")
 
+# separate M/D/Y from time
 img_time$Day <- format(img_time$Time, format = "%m/%d/%Y")
 
+# make M/D/Y a date
 img_time$Day <- as.Date(img_time$Day, format = "%m/%d/%Y")
 
+# Correct for daylight savings, imageJ and mapping file times won't match during these times
 img_time$Time_Corr <- ifelse(img_time$Day < "2022-11-06" | img_time$Day > "2023-03-12", img_time$Time + 3600, img_time$Time)
 
+# correct to POSIXct
 class(img_time$Time_Corr) <- c("POSIXct", "POSIXt")
 
+# format Time as H:M:S
 img_time$Time_HMS <- format(as.POSIXct(img_time$Time_Corr), format = "%H:%M:%S")
 
+# Pull out H:M - this as the best resolution we get from mapping file
 img_time$Time_HM <- format(as.POSIXct(img_time$Time_Corr), format = "%H:%M")
 
+# pull out second picture is taken - use this if picture was taken early on in minute
 img_time$Time_S <- format(as.POSIXct(img_time$Time_Corr), format = "%S")
 
+#merge time of pictures with 'Time on' from mapping files
 all.times <- merge(img_time, all.map.clean) %>% 
   drop_na(`Time on`)
 
+# flag what the minute before would be
 all.times$min_bef <- format(strptime(all.times$Time_HM, format = "%H:%M") - 60, "%H:%M")
 
 all.times$time_same <- NA
@@ -192,6 +206,7 @@ for (i in 1:nrow(all.times)) {
   
   else if (all.times$Time_S[i] <= 20 & all.times$`Time on`[i] == all.times$min_bef[i]) {
     
+    # if picture taken < 20 s after new minute, sample was maybe put on near start
     all.times$time_same[i] <- "maybe"
     
   }
@@ -212,6 +227,7 @@ corr.time <- all.times %>%
   arrange(Time_HM) %>% 
   filter(row_number() == 1) %>% 
   rename(Sample_Name = Sample_ID) %>% 
+  ungroup() %>% 
   mutate(Elapsed_Minute = 2)
 
 samples <- merge(corr.time, cleaned_data, by = c("Sample_Name", "Elapsed_Minute"), all = TRUE) %>% 
@@ -238,7 +254,7 @@ for (i in 1:nrow(samples)){
 samples_clean = samples %>% 
   unite(Sample_Name, c("EC", "kit", "rep"), sep = "_") %>% 
   arrange(Sample_Name) %>% 
-  mutate(DO_mg_per_L = round(DO_mg_per_L, 2)) %>% 
+  mutate(DO_mg_per_L = round(DO_mg_per_L, 2)) %>% # round to 2 for data package publishing
   relocate(Sample_Name, .before = Elapsed_Minute)
 
 output.name = paste0(study.code,"INC_Raw_DO_By_Min_ReadyForBoye_",Sys.Date(),".csv")
