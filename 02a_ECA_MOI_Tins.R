532cb3533a7a54f431858df71580fd5595c5723f
mmlaan <maggi.laan@gmail.com>
2024-07-18 09:38:38 -0700

    Bibi informal code review comments

--- 02a_ECA_MOI_Tins.R
+++ 02a_ECA_MOI_Tins.R
@@ -10,28 +10,29 @@ study.code = 'EC'
 year = '2022'
 
 # Set working directory to data file
-  #For Macbook Users
-#data.in.path <- file.path("/Users/delg580/Library/CloudStorage/OneDrive-SharedLibraries-PNNL/Core Richland and Sequim Lab-Field Team - Documents/Data Generation and Files/ECA/MOI/01_RawData")
-
-#data.out.path <- file.path("/Users/delg580/Library/CloudStorage/OneDrive-SharedLibraries-PNNL/Core Richland and Sequim Lab-Field Team - Documents/Data Generation and Files/ECA/MOI/03_ProcessedData")
 
 #For Windows Users
-
 data.out.path <- file.path("C:/Users/",pnnl.user,"/PNNL/Core Richland and Sequim Lab-Field Team - Documents/Data Generation and Files/ECA/MOI/03_ProcessedData")
+
+#For Mac Users 
+#data.out.path <- file.path("/Users/",pnnl.user,"/Library/CloudStorage/OneDrive-SharedLibraries-PNNL/Core Richland and Sequim Lab-Field Team - Documents/Data Generation and Files/ECA/MOI/03_ProcessedData")
 
 # Load Data
 moisture <- read_excel(paste0("C:/Users/",pnnl.user,"/PNNL/Core Richland and Sequim Lab-Field Team - Documents/Data Generation and Files/ECA/MOI/01_RawData/",year,"_Data_Raw_MOI_ECA_",study.code,".xlsx")) %>% janitor::clean_names()
 
+#For Macbook Users
+#moisture <- read_excel(paste0("/Users/",pnnl.user,"/Library/CloudStorage/OneDrive-SharedLibraries-PNNL/Core Richland and Sequim Lab-Field Team - Documents/Data Generation and Files/ECA/MOI/01_RawData/",year,"_Data_Raw_MOI_ECA_",study.code,".xlsx")) %>% janitor::clean_names()
+
 # Subsetting data by wet weight
 wet_weight <- moisture %>% 
-  dplyr::select(sample_name, date, tare_weight_g, sample_weight_g) %>% 
+  dplyr::select(sample_name, date, tare_weight_g, sample_weight_g_tared) %>% 
   filter(!grepl("-9999" , tare_weight_g))
 
 colnames(wet_weight) [4] <- "wet_weight_g"
 
 # Subsetting data by dry weight
 dry_weight <- moisture %>% 
-  dplyr::select(sample_name, date, tare_weight_g, sample_weight_g) %>% 
+  dplyr::select(sample_name, date, tare_weight_g, sample_weight_g_tared) %>% 
   filter(grepl("-9999", tare_weight_g))
 
 colnames(dry_weight) [4] <- "dry_weight_g"
@@ -39,7 +40,7 @@ colnames(dry_weight) [4] <- "dry_weight_g"
 
 dim(wet_weight); length(unique(wet_weight$sample_name)) #number of unique entries
 dim(dry_weight); length(unique(dry_weight$sample_name)) #number of unique entries
-length(which(wet_weight$sample_name %in% dry_weight$sample_name)) #which ID's in wet weight file are found in the dry weight file
+length(which(wet_weight$sample_name %in% dry_weight$sample_name)) #which ID's in wet weight file are found in the dry weight file (missing EC_23 missing wet weight)
 
 # Merged wet and dry weights by sample name
 merged_weights <- merge(x = wet_weight, y = dry_weight, by = "sample_name", all = TRUE) %>% janitor::clean_names()
@@ -47,11 +48,12 @@ dim(merged_weights)
 head(merged_weights)
 names(merged_weights)
 
-# Calculate True dry weight 
+# Calculate True dry weight ((dry weight + moisture tin) - moisture tin)
 merged_weights$true_dry_weight_g <- merged_weights$dry_weight_g - merged_weights$tare_weight_g_x 
-range(merged_weights$true_dry_weight_g)
 
-# Calculatee Gravimetric Water Content
+
+# Calculate Gravimetric Water Content 
+#(water mass/dry sediment mass)*100
 merged_weights$percent_water_content_dry <- ((merged_weights$wet_weight_g - merged_weights$true_dry_weight_g)/merged_weights$true_dry_weight_g)*100
 
 
@@ -59,14 +61,10 @@ merged_weights$percent_water_content_dry <- ((merged_weights$wet_weight_g - merg
 cols_to_drop <- c ("date_x","date_y", "tare_weight_g_y","tare_weight_g_x", "dry_weight_g" )
 merged_weights <- merged_weights[ , !(names(merged_weights) %in% cols_to_drop)]
 
-range(merged_weights$percent_water_content_dry)
-
-merged_weights$flag[is.na(merged_weights$wet_weight_g)] = "Missing Wet Weight"
-
+# Add extra 0 to missing ones in ECA Names
 merged_weights <- merged_weights %>% 
   separate(sample_name, c("EC", "Site", "Rep"), sep = "_")
 
-# Add extra 0 to missing ones in ECA Names
 for (i in 1:nrow(merged_weights)){
   
   if (str_count(merged_weights$Site[i], "[0-9]") <= 2){
@@ -82,58 +80,59 @@ for (i in 1:nrow(merged_weights)){
   
 }
 
-## Clean Data 
-  ## EC: 
-  
-  ## EV_003, EV_004 - not publishing this data
-  ## EV_020_MOI-3: Spilled, needs to be removed
+merged_weights <- merged_weights %>% 
+  unite("Sample_Name", c("EC", "Site", "Rep"), sep = "_")
 
+## Get average moisture content and stdev of moisture content from replicates 
+  
 eca_grav <- merged_weights %>% 
-  unite("Sample_Name", c("EC", "Site", "Rep"), sep = "_") %>% 
-  #filter(!grepl("EV_003|EV_004|EV_020_MOI-3", Sample_Name)) %>% 
   separate(Sample_Name, c("Sample_Site", "Rep"), sep = "-", remove = FALSE) %>% 
   separate(Sample_Site, c("EC", "Site", "MOI"), sep = "_") %>% 
-  mutate(grav_eca = ((wet_weight_g - true_dry_weight_g)/true_dry_weight_g)*100) %>% 
   group_by(Site) %>% 
-  mutate(average_eca = mean(grav_eca)) %>% 
-  mutate(cv_eca = (sd(grav_eca)/mean(grav_eca))*100) %>%   dplyr::select(c("Sample_Name", "Site", "Rep", "grav_eca", "average_eca", "cv_eca"))
+  mutate(average_eca = mean(percent_water_content_dry)) %>% 
+  mutate(cv_eca = (sd(percent_water_content_dry)/mean(percent_water_content_dry))*100) %>%  
+  dplyr::select(c("Sample_Name", "Site", "Rep", "wet_weight_g", "true_dry_weight_g", "percent_water_content_dry", "average_eca", "cv_eca")) %>% 
+  ungroup()
 
-## Remove Outliers if Needed ####
+## Remove Obivous Outliers (those with notes) if Needed ####
 
 ## EC: 
 
-## EC_057_MOI-3 spilled before weighing
-## EC_084_MOI-2 typo? negative value
+# EC_057_MOI-3 spilled before weighing
+# EC_084_MOI-2 typo? negative value
 
 ## EV: highest CV is 45%, but no distinct outliers
+# EV_003, EV_004 - not publishing this data, sampled twice 
+# EV_020_MOI-3: Spilled, needs to be removed
 
-merged_weights_average = merged_weights %>%
-  #filter(Sample_Name != "EC_057_MOI-3") %>% 
-  #filter(Sample_Name != "EC_084_MOI-2") %>% 
-  separate(Sample_Name, c("EC", "Site", "Rep"), sep = "_") %>% 
+eca_grav_rem = eca_grav %>%
+  filter(Sample_Name != "EC_057_MOI-3") %>% 
+  filter(Sample_Name != "EC_084_MOI-2") %>% 
+  filter(!grepl("EV_003|EV_004|EV_020_MOI-3", Sample_Name)) %>% # use this when processing EV data
   group_by(Site) %>% 
-  mutate(average = mean(percent_water_content_dry)) %>% 
-  mutate(cv = sd(percent_water_content_dry)/mean(percent_water_content_dry))
+  mutate(average_rem = mean(percent_water_content_dry)) %>% 
+  mutate(cv_rem = sd(percent_water_content_dry)/mean(percent_water_content_dry)*100) %>% 
+  ungroup() %>% 
+  right_join(eca_grav, by = "Sample_Name") %>%
+  mutate(flag = if_else(is.na(wet_weight_g.y),  "Missing Wet Weight", if_else(is.na(average_rem), "removed outlier", "NA"))) # Add flag to sites missing wet weight then flag removed samples
 
-
+# Keep average site moisture and look at histogram
 eca_cv <- eca_grav %>% 
   distinct(Site, .keep_all = TRUE)
 
 ggplot(eca_cv, aes(x = cv_eca)) +
   geom_histogram()
 
-
-clean_eca <- merged_weights %>% 
-  filter(!grepl("EV_003|EV_004|EV_020_MOI-3", Sample_Name)) %>% 
-  mutate(Gravimetric_Moisture = ((wet_weight_g - true_dry_weight_g)/true_dry_weight_g)*100) %>% 
-  dplyr::select(c(Sample_Name, wet_weight_g, true_dry_weight_g, percent_water_content_dry, Gravimetric_Moisture, flag)) %>% 
-  rename(Wet_Sediment_Mass_g = wet_weight_g) %>% 
-  rename(Dry_Sediment_Mass_g = true_dry_weight_g) %>% 
+# Make final DF for publishing/running through outlier script
+clean_eca <- eca_grav_rem %>% 
+ rename(Gravimetric_Moisture = percent_water_content_dry.y) %>% 
+  rename(Wet_Sediment_Mass_g = wet_weight_g.y) %>%
+  rename(Dry_Sediment_Mass_g = true_dry_weight_g.y) %>% 
   mutate(Water_Mass_g = Wet_Sediment_Mass_g - Dry_Sediment_Mass_g) %>% 
   mutate(Gravimetric_Moisture = round(Gravimetric_Moisture, 2)) %>% 
   mutate(Methods_Deviation = "N/A") %>% 
- # mutate(Sample_Name = str_replace(Sample_Name, "EC", "CM")) %>% 
-  dplyr::select(c(Sample_Name, Wet_Sediment_Mass_g, Dry_Sediment_Mass_g, Water_Mass_g, Gravimetric_Moisture, flag, Methods_Deviation))
+  mutate(Sample_Name = str_replace(Sample_Name, "EC", "CM")) %>% #change EC to CM for data publishing
+  dplyr::select(c(Sample_Name, Gravimetric_Moisture, Wet_Sediment_Mass_g, Dry_Sediment_Mass_g, Water_Mass_g, flag, Methods_Deviation))
 
 write.csv(clean_eca, paste0("C:/Users/",pnnl.user,"/PNNL/Core Richland and Sequim Lab-Field Team - Documents/Data Generation and Files/ECA/MOI/03_ProcessedData/",study.code,"_MOI_ReadyForBoye_", Sys.Date(),".csv"), row.names = F)
 
